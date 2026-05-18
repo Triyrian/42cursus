5a873c82a74d05cbb08c60d2d1760d712fbdf643
victoriantriyadi <vtriyadi@k2r2p10.42mulhouse.fr>
2026-05-18 18:02:25 +0200

    push

--- py6/alchemy/potions.py
+++ py6/alchemy/potions.py
@@ -4,10 +4,10 @@ import alchemy.elements
 
 def healing_potion() -> str:
     return "Healing potion brewed with "\
-           f"’{alchemy.elements.create_earth()}’ and "\
-           f"’{alchemy.elements.create_air()}'"
+           f"'{alchemy.elements.create_earth()}' and "\
+           f"'{alchemy.elements.create_air()}'"
 
 
 def strength_potion() -> str:
     return "Strength potion brewed "\
-           f"with ’{elements.create_fire()}’ and ’{elements.create_water()}'"
+           f"with '{elements.create_fire()}' and '{elements.create_water()}'"
